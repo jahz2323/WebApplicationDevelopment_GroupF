320fee54fa61e8fde3dfbf0b863dc6dbe05b7f15
jahz2323 <jahzielbelmonte@gmail.com>
2025-03-21 11:40:11 +0000

    Started Development on Homepage.html, just the static stuff no js implemented at the moment,

--- ACMEMC_WebApplication/settings.py
+++ ACMEMC_WebApplication/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Bootstrap dependency
+    "bootstrap5",
+    # Chartjs dependency 
+    'chartjs'
 ]
 
 MIDDLEWARE = [
