95e06c1b2aba64f6f4fb4390a8c726090fa7ae18
Thomas Viard <thomas.viard@epitech.eu>
2025-04-04 23:53:12 +0100

    Adds initial project setup with Docker
    Configures the project for containerization using Docker, including Dockerfile and docker-compose.yml.
    
    Sets up initial database and loads fixtures for collections, users, machineries and machineryfaults.
    
    Also, includes .dockerignore to exclude unnecessary files and directories and adds a sample .env file.
    
    Removes unused import from admin.py

---
+++ docker_entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+# Move to application directory
+cd /app
+
+# Run any new database migrations
+python manage.py migrate
+
+python manage.py loaddata App/fixtures/collections.json
+python manage.py loaddata App/fixtures/users.json
+python manage.py loaddata App/fixtures/machineries.json
+python manage.py loaddata App/fixtures/machineryfaults.json
+
+# Run Django’s development server
+python manage.py runserver 0.0.0.0:8000
