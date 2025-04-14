b01b7a9f0461ce5a4f76de88ce70a85fe2453c8e
Jahziel Belmonte <38128644+jahz2323@users.noreply.github.com>
2025-04-14 10:21:05 +0100

    Update urls.py - FAULT CASE DETAILS WAS COMMENTED OUT

--- App/urls.py
+++ App/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path("register/", views.user_registration, name="user_registration"),
     path("submit-registration/", views.register_user, name="register_user"),
     path("register/success/", views.registration_success, name="registration_success"),
-    # path("FaultCaseDetails <int:machinery_id>/", views.FaultCaseDetails, name="FaultCaseDetails"),
+    path("FaultCaseDetails <int:machinery_id>/", views.FaultCaseDetails, name="FaultCaseDetails"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
